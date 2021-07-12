e3be2d28d819b25ee5339da2250424c3338060af
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-12 17:12:17 +0900

    変数は全てデフォルトでimmutable！

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod vars; //別モジュールからのインポート
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     vars::run(); //コロン２つでそのモジュールの階層を移動できる。また、モジュールはデフォルトでプライベートになっているのでパブリックにする必要がある。
-    vars::sub_a::func_a();
+
+    // vars::sub_a::func_a();
 }
--- src/vars.rs
+++ src/vars.rs
@@ -1,8 +1,14 @@
-pub mod sub_a; //サブモジュールを親でも使いたい場合はpubをつける
-mod sub_b;
+// pub mod sub_a; //サブモジュールを親でも使いたい場合はpubをつける
+// mod sub_b;
 
 pub fn run() {
   println!("Here is vars module");
-  sub_a::func_a();
-  sub_b::func_b();
+  // sub_a::func_a();
+  // sub_b::func_b();
+  let x = 5;
+  // x = 6; //デフォルトでは変数は全てimmutable(不変)
+  println!("The value of x is :{}", x);
+  let mut y = 3;
+  y = 4; //変えたいならmutを付ける。
+  println!("The value of y is :{}", y);
 }
